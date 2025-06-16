032737c82f423a7ea11568de31a3d978ba5d769a
Vincent Rémond <vincent.remond@gmail.com>
2025-06-16 17:48:16 +0200

    v2

--- Pinicola.HotKeys/App.xaml.cs
+++ Pinicola.HotKeys/App.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System.Runtime.CompilerServices;
-using System.Windows;
-using System.Windows.Input;
-using Dapplo.Windows.User32;
-using Dapplo.Windows.User32.Enums;
-using NHotkey;
-using NHotkey.Wpf;
+﻿using System.Windows;
 
 namespace Pinicola.HotKeys;
 
@@ -15,41 +9,7 @@ public partial class App
 {
     protected override void OnStartup(StartupEventArgs e)
     {
-        Register(ModifierKeys.Alt | ModifierKeys.Shift | ModifierKeys.Control, Key.Down, Minimize);
-        Register(ModifierKeys.Alt | ModifierKeys.Shift | ModifierKeys.Control, Key.Up, Maximize);
+        HookRegister.Run();
         base.OnStartup(e);
-    }
-
-    private void Register(ModifierKeys modifiers, Key key, EventHandler<HotkeyEventArgs> eventHandler, [CallerArgumentExpression(nameof(eventHandler))] string? callerName = null)
-    {
-        HotkeyManager.Current.AddOrReplace(
-            $"Pinicola.HotKeys.{callerName}",
-            key,
-            modifiers,
-            eventHandler
-        );
-    }
-
-    private void Maximize(object? sender, HotkeyEventArgs e)
-    {
-        ShowOrHideForegroundWindow(ShowWindowCommands.Maximize);
-        e.Handled = true; // Prevent further processing of this hotkey
-    }
-
-    private void Minimize(object? sender, HotkeyEventArgs e)
-    {
-        ShowOrHideForegroundWindow(ShowWindowCommands.Minimize);
-        e.Handled = true; // Prevent further processing of this hotkey
-    }
-
-    private static void ShowOrHideForegroundWindow(ShowWindowCommands command)
-    {
-        var foregroundWindowHandle = User32Api.GetForegroundWindow();
-        if (foregroundWindowHandle == IntPtr.Zero)
-        {
-            return;
-        }
-
-        User32Api.ShowWindow(foregroundWindowHandle, command);
     }
 }
---
+++ Pinicola.HotKeys/HookRegister.cs
@@ -0,0 +1,63 @@
+﻿using Dapplo.Windows.Input.Enums;
+using Dapplo.Windows.Input.Keyboard;
+using Dapplo.Windows.User32;
+using Dapplo.Windows.User32.Enums;
+
+namespace Pinicola.HotKeys;
+
+public static class HookRegister
+{
+    public static void Run()
+    {
+        Register(
+            Maximize,
+            VirtualKeyCode.LeftWin,
+            VirtualKeyCode.LeftShift,
+            VirtualKeyCode.Up
+        );
+
+        Register(
+            Minimize,
+            VirtualKeyCode.LeftWin,
+            VirtualKeyCode.LeftShift,
+            VirtualKeyCode.Down
+        );
+    }
+
+    private static void Register(Action action, params VirtualKeyCode[] keyCodes)
+    {
+        var keyCombinationHandler = new KeyCombinationHandler(keyCodes)
+        {
+            IgnoreInjected = false,
+        };
+
+        KeyboardHook.KeyboardEvents.Where(keyCombinationHandler).Subscribe(x =>
+            {
+                x.Handled = true;
+                action();
+            }
+        );
+    }
+
+    private static void Maximize()
+    {
+        ShowOrHideForegroundWindow(ShowWindowCommands.Maximize);
+    }
+
+    private static void Minimize()
+    {
+        ShowOrHideForegroundWindow(ShowWindowCommands.Minimize);
+    }
+
+
+    private static void ShowOrHideForegroundWindow(ShowWindowCommands command)
+    {
+        var foregroundWindowHandle = User32Api.GetForegroundWindow();
+        if (foregroundWindowHandle == IntPtr.Zero)
+        {
+            return;
+        }
+
+        User32Api.ShowWindow(foregroundWindowHandle, command);
+    }
+}
