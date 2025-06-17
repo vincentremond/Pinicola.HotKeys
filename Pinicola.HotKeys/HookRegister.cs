f743570fb8499469d8178c043f6b62a69388f244
Vincent Rémond <vincent.remond@gmail.com>
2025-06-17 14:20:50 +0200

    Refactor HookRegister to improve key registration and add SendTimeStamp functionality; update build script to include debug path

--- Pinicola.HotKeys/HookRegister.cs
+++ Pinicola.HotKeys/HookRegister.cs
@@ -1,4 +1,5 @@
-﻿using Dapplo.Windows.Input.Enums;
+﻿using System.Reactive.Linq;
+using Dapplo.Windows.Input.Enums;
 using Dapplo.Windows.Input.Keyboard;
 using Dapplo.Windows.User32;
 using Dapplo.Windows.User32.Enums;
@@ -11,32 +12,80 @@ public static class HookRegister
     {
         Register(
             Maximize,
-            VirtualKeyCode.LeftWin,
-            VirtualKeyCode.LeftShift,
-            VirtualKeyCode.Up
+            [
+                VirtualKeyCode.LeftWin, VirtualKeyCode.LeftShift,
+                VirtualKeyCode.Up,
+            ]
         );
 
         Register(
             Minimize,
-            VirtualKeyCode.LeftWin,
-            VirtualKeyCode.LeftShift,
-            VirtualKeyCode.Down
+            [
+                VirtualKeyCode.LeftWin, VirtualKeyCode.LeftShift,
+                VirtualKeyCode.Down,
+            ]
+        );
+        Register(
+            SendTimeStamp,
+            [
+                VirtualKeyCode.LeftControl, VirtualKeyCode.LeftWin,
+                VirtualKeyCode.Menu, VirtualKeyCode.KeyT,
+            ]
         );
     }
 
-    private static void Register(Action action, params VirtualKeyCode[] keyCodes)
+    private static void SendTimeStamp()
+    {
+        var timestamp = DateTime.Now.ToString("yyyy-MM-dd--HH-mm-ss");
+        var keycodes = ToKeyCodes(timestamp);
+        KeyboardInputGenerator.KeyPresses(keycodes);
+    }
+
+    private static VirtualKeyCode[] ToKeyCodes(string input)
+    {
+        var keyCodes = new List<VirtualKeyCode>(input.Length);
+        foreach (var c in input)
+        {
+            if (char.IsLetterOrDigit(c))
+            {
+                keyCodes.Add((VirtualKeyCode)char.ToUpper(c));
+            }
+            else if (c == '-')
+            {
+                keyCodes.Add(VirtualKeyCode.OemMinus);
+            }
+            else if (c == ':')
+            {
+                keyCodes.Add(VirtualKeyCode.Oem1);
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException(nameof(c), c, message: null);
+            }
+        }
+
+        return keyCodes.ToArray();
+    }
+
+    private static void Register(Action action, VirtualKeyCode[] keyCodes, bool keyDown = true)
     {
         var keyCombinationHandler = new KeyCombinationHandler(keyCodes)
         {
             IgnoreInjected = false,
         };
 
-        KeyboardHook.KeyboardEvents.Where(keyCombinationHandler).Subscribe(x =>
-            {
-                x.Handled = true;
-                action();
-            }
-        );
+        KeyboardHook.KeyboardEvents.Where(keyCombinationHandler)
+            .Subscribe(x =>
+                {
+                    if (!(x.IsKeyDown && keyDown))
+                    {
+                        return;
+                    }
+
+                    action();
+                    x.Handled = true;
+                }
+            );
     }
 
     private static void Maximize()
